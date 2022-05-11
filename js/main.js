3841bfaadede3242bdaf258c5de9d9d38c030ca5
Ahmedtaher820 <et8989784@gmail.com>
2022-05-11 15:00:32 +0200

    first update

--- js/main.js
+++ js/main.js
@@ -1,50 +1,24 @@
-var miniNav = document.getElementById("mini-nav");
-var mainNav = document.getElementById("main-nav");
-var brandChar = document.getElementById("brand-char");
-window.addEventListener("scroll", function () {
-  miniNav.classList.toggle("active", window.scrollY > 0);
-  mainNav.classList.toggle("active", window.scrollY > 0);
-  if (window.scrollY > 0) {
-    brandChar.style.color = "white";
-  } else {
-    brandChar.style.color = "#343148";
-  }
-});
-$(".shedule ul li").on("click", function () {
-  $(this).addClass("active").siblings().removeClass("active");
-  if ($(this).data("class") === "All") {
-    $(".classes .col-md-6").css("display", "block");
-  } else {
-    $(".classes .col-md-6").css("display", "none");
-    $($(this).data("class")).parent().css("display", "block");
-  }
-});
-
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  responsive: {
-    0: {
-      items: 1,
-    },
-    600: {
-      items: 3,
-    },
-    1000: {
-      items: 3,
-    },
-  },
-});
-// goup button
-let goup = document.querySelector(".goup");
-window.addEventListener("scroll", () => {
-    if(window.pageYOffset > 600){
-        goup.classList.add("active")
-    }else{
-        goup.classList.remove("active")
+var miniNav= document.getElementById("mini-nav");
+var mainNav= document.getElementById("main-nav");
+var brandChar= document.getElementById("brand-char");
+    window.addEventListener("scroll",function(){
+    miniNav.classList.toggle("active" , window.scrollY > 0);
+    mainNav.classList.toggle("active" , window.scrollY > 0);
+        if(window.scrollY > 0){
+            brandChar.style.color = "white"
+        }
+        else{
+            brandChar.style.color = "#343148"
+        }
+})
+$('.shedule ul li').on('click',function(){
+    $(this).addClass('active').siblings().removeClass('active');
+    if($(this).data('class') === 'All'){
+        $('.classes .col-md-6').css('display','block');
+    }
+    else{
+       $('.classes .col-md-6').css('display','none');
+        $($(this).data('class')).parent().css('display','block')
     }
 });
-goup.onclick = () => {
-  window.scrollTo(0, 0);
-};
+
